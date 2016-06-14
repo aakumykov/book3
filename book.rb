5223ad13b5088cf7386199a258aa3e4f84dc8be8
Андрей Кумыков <aakumykov@yandex.ru>
2016-06-14 09:33:12 +0700

    Заглушки методов add_source, prepare, save

--- book.rb
+++ book.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 #coding: UTF-8
 
+
 class Book
 	attr_reader :title, :author, :language
 
@@ -8,6 +9,8 @@ class Book
 		@title = 'Новая книга'
 		@author = 'Неизвестный автор'
 		@language = 'ru'
+
+		@source = []
 	end
 
 	def title=(a_title)
@@ -25,5 +28,33 @@ class Book
 		raise 'некорректный язык книги' if not a_language.match(/^[a-z]{2}$/)
 		@language = a_language
 	end
+
+	def add_source(src)
+		@source << src.to_s
+	end
+
+	def prepare
+		puts "#{self.class}.#{__method__}()"
+	end
+
+	def save
+		puts "#{self.class}.#{__method__}()"
+	end
 end
+
+
+book = Book.new
+
+book.title = 'Пробная книга'
+book.author = 'Кумыков Андрей'
+book.language = 'ru'
+
+book.add_source 'http://opennet.ru'
+book.add_source 'http://geektimes.ru'
+
+book.prepare
+
+book.save
+
+puts book.inspect
 
