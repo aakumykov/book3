ee8d8912a5e7a6655548a79c0923eaa936814cb2
Андрей Кумыков <aakumykov@yandex.ru>
2016-06-22 16:27:59 +0700

    Метод load_page()

--- book.rb
+++ book.rb
@@ -2,6 +2,7 @@
 #coding: UTF-8
 
 require 'sqlite3'
+require 'net/http'
 
 class Book
 	attr_reader :title, :author, :language
@@ -91,14 +92,17 @@ class Book
 	end
 
 	def process_next_page
-		puts "#{self.class}.#{__method__}()"
+		Msg::debug("#{self.class}.#{__method__}()")
 		
 		@page_count += 1
 
 		lnk = get_next_link
+		#Msg::debug("следующая ссылка: #{lnk}")
+		
 		# rules = find_rules(lnk)
 
-		# raw_page = load_page(lnk)
+		raw_page = load_page(lnk)
+		Msg::debug "размер страницы: #{raw_page.lines.count} строк / #{raw_page.bytes.count} байт"
 
 		# collect_links(raw_page, rules)
 
@@ -112,11 +116,58 @@ class Book
 		puts "#{self.class}.#{__method__}()"
 		
 		res = @@db.execute("SELECT uri FROM #{@@table_name} WHERE processed=0 LIMIT 1")
-		uri = res.first.first
+		res.first.first
+	end
+	
+	def load_page(uri)
+		puts "#{__method__}(#{uri})"
+
+		redirects_limit = 3
 		
-		Msg::debug("свежая ссылка: #{uri}")
+		raise ArgumentError, 'слишком много перенаправлений' if redirects_limit == 0
 		
-		uri
+		#uri = URI.escape(uri) if not uri.urlencoded?
+		uri = URI(uri)
+
+		data = {}
+
+		Net::HTTP.start(uri.host, uri.port, :use_ssl => 'https'==uri.scheme) { |http|
+
+		  request = Net::HTTP::Get.new(uri.request_uri)
+		  request['User-Agent'] = 'Mozilla/5.0 (X11; Linux i686; rv:39.0) Gecko/20100101 Firefox/39.0'
+		  
+		  response = http.request request
+
+		  case response
+			when Net::HTTPRedirection then
+				location = response['location']
+				puts "перенаправление на '#{location}'"
+				data =  self.work(
+					:uri => location, 
+					:redirects_limit => (redirects_limit-1)
+				)
+			when Net::HTTPSuccess then
+				data = {
+					:headers => response.to_hash,
+					:page => response.body,
+				}
+			else
+				response.value
+		  end
+		}
+	  
+		# перекодировка в UTF-8
+		#charset = detectCharset(data)
+		#puts "charset: #{charset}"
+		# page = data[:page].encode(
+		# 	'UTF-8', 
+		# 	charset, 
+		# 	{ :replace => '_', :invalid => :replace, :undef => :replace }
+		# )
+
+		page = data[:page]
+	  
+		return page
 	end
 
 	def save
@@ -127,7 +178,7 @@ end
 
 class Msg
 	def self.debug(msg)
-		puts "ОТЛАДКА: #{msg}"
+		puts msg
 	end
 	
 	def self.info(msg)
