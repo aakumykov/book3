4de9a18ae47d5b28b2747be0929d0230c1a7cec8
Андрей Кумыков <aakumykov@yandex.ru>
2016-07-02 13:37:57 +0700

    Продолжаю писать, методы

--- rules/opennet_ru.rb
+++ rules/opennet_ru.rb
@@ -1,9 +1,10 @@
+# coding: utf-8
 
 class OpennetRu
 	@@link_names = {
 		main_page: '^http://opennet\.ru$',
 		news_article: '/opennews/art\.shtml\?num=[0-9]+$',
-		unknown_page: '^.+$'
+		any_page: '^.+$'
 	}
 
 	@@rules = {
@@ -13,18 +14,40 @@ class OpennetRu
 		},
 		news_article: {
 		},
-		unknown_page: {
-			
+		any_page: {
+			any_page: :DefaultPage,
 		},
 	}
 
-	def DefaultPage
+	def initialize
+		link_names = @@link_names.sort_by { |name,pattern|
+			pattern.length
+		}.reverse.to_h
+		
+		#puts "#{link_names}"
+		#link_names.each { |k,v| puts "#{k} => #{v}"}
 	end
 
-	def MainPage
+	def accept_link?(uri)
+		
 	end
 
-	def NewsArticle
+
+	private
+
+	# Страничные методы
+	def DefaultPage(dom)
+		dom.search('//body').to_html
+	end
+
+	def MainPage(dom)
+		dom.search('//body//table')[2].to_html
+	end
+
+	def NewsArticle(dom)
+		dom.search("//form[@action='/cgi-bin/openforum/ch_cat.cgi']").inner_html
 	end
 end
 
+
+o = OpennetRu.new
