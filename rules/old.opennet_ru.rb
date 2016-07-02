ca1d3ae9176b0dc09e06385d45abe1ede77438d2
Андрей Кумыков <aakumykov@yandex.ru>
2016-07-02 12:19:01 +0700

    Пишу новый файл настроек

---
+++ rules/old.opennet_ru.rb
@@ -0,0 +1,59 @@
+#coding: UTF-8
+
+class OpennetRu
+	@@rules = {
+		'^http://opennet\.ru$' => :MainPage,
+		'/opennews/art\.shtml\?num=[0-9]+$' => :NewsArticle,
+	}
+	
+	# инициализация
+	def initialize
+		@link_patterns = @@rules.keys.sort_by { |k| k.length }.reverse
+		#Msg::debug "@link_patterns: #{@link_patterns}"
+		#Msg::debug "@@rules: #{@@rules}"
+	end
+	
+	def accept_link?(uri)
+		accept = false
+		
+		begin
+			@link_patterns.each { |pattern|
+				raise 'match' if uri.match(pattern)
+			}
+		rescue
+			accept = true
+		end
+		
+		accept
+	end
+	
+	def get_processor(uri)
+		Msg::debug("#{self.class}.#{__method__}(#{uri})", nobr: true)
+		
+		processor_name = nil
+		
+		begin
+			@link_patterns.each { |pattern|
+				#Msg::debug " #{uri}, (#{pattern})"
+				raise pattern if uri.match(pattern)
+			}
+		rescue => e
+			processor_name = @@rules[e.message]
+			
+				Msg::debug " -> processor_name: #{processor_name} (#{processor_name.class})"
+		end
+		
+		processor_name
+	end
+	
+	# "страничные" методы
+	def MainPage(page)
+		Msg::debug("#{self.class}.#{__method__}(page.size: #{page.size})")
+		page
+	end
+	
+	def NewsArticle(page)
+		Msg::debug("#{self.class}.#{__method__}(page.size: #{page.size})")
+		page
+	end
+end
--- rules/opennet_ru.rb
+++ rules/opennet_ru.rb
@@ -1,59 +1,30 @@
-#coding: UTF-8
 
 class OpennetRu
-	@@rules = {
-		'^http://opennet\.ru$' => :MainPage,
-		'/opennews/art\.shtml\?num=[0-9]+$' => :NewsArticle,
+	@@link_names = {
+		main_page: '^http://opennet\.ru$',
+		news_article: '/opennews/art\.shtml\?num=[0-9]+$',
+		unknown_page: '^.+$'
 	}
-	
-	# инициализация
-	def initialize
-		@link_patterns = @@rules.keys.sort_by { |k| k.length }.reverse
-		#Msg::debug "@link_patterns: #{@link_patterns}"
-		#Msg::debug "@@rules: #{@@rules}"
-	end
-	
-	def accept_link?(uri)
-		accept = false
-		
-		begin
-			@link_patterns.each { |pattern|
-				raise 'match' if uri.match(pattern)
-			}
-		rescue
-			accept = true
-		end
-		
-		accept
-	end
-	
-	def get_processor(uri)
-		Msg::debug("#{self.class}.#{__method__}(#{uri})", nobr: true)
-		
-		processor_name = nil
-		
-		begin
-			@link_patterns.each { |pattern|
-				#Msg::debug " #{uri}, (#{pattern})"
-				raise pattern if uri.match(pattern)
-			}
-		rescue => e
-			processor_name = @@rules[e.message]
+
+	@@rules = {
+		main_page: {
+			news_article: :NewsArticle,
+			main_page: :MainPage,
+		},
+		news_article: {
+		},
+		unknown_page: {
 			
-				Msg::debug " -> processor_name: #{processor_name} (#{processor_name.class})"
-		end
-		
-		processor_name
+		},
+	}
+
+	def DefaultPage
 	end
-	
-	# "страничные" методы
-	def MainPage(page)
-		Msg::debug("#{self.class}.#{__method__}(page.size: #{page.size})")
-		page
+
+	def MainPage
 	end
-	
-	def NewsArticle(page)
-		Msg::debug("#{self.class}.#{__method__}(page.size: #{page.size})")
-		page
+
+	def NewsArticle
 	end
 end
+
